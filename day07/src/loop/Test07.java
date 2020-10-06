978bc4797e29b397c9d50407544c823b6a966f9b
YR <YR@DESKTOP-HIAR6MN>
2020-10-06 22:11:41 +0900

    day7(반복문)

--- day07/src/loop/Test07.java
+++ day07/src/loop/Test07.java
@@ -7,9 +7,9 @@ public class Test07 {
 	public static void main(String[] args) {
 		
 	for(int i=1; i<100; i++) {
-		if((int)(i/10)==3 || (i%10)==3
-		 ||(int)(i/10)==6 || (i%10)==6
-		 ||(int)(i/10)==9 || (i%10)==9) {
+		if((i/10)==3 || (i%10)==3
+		 ||(i/10)==6 || (i%10)==6
+		 ||(i/10)==9 || (i%10)==9) {
 			System.out.println(i);
 			}
 		}
