46630a4cdc920a837558966d233915bf294c6c5f
YR <YR@DESKTOP-HIAR6MN>
2020-10-14 17:01:15 +0900

    day11(test)

--- day11/src/oop/basic4/Coffee.java
+++ day11/src/oop/basic4/Coffee.java
@@ -1,6 +1,7 @@
 package oop.basic4;
 
 public class Coffee {
+	//인스턴스변수, 멤버변수 : 객체가 사라지기 전까지 존재
 	String name,type;
 	int price;
 	boolean event;
@@ -8,12 +9,7 @@ public class Coffee {
 	void info(String a, String b, int c,boolean d){
 		this.name=a;
 		this.type=b;
-		if(d==true) {
-			this.price=c-500;
-			}
-		else {
-			this.price=c;
-			}
+		this.price=c;
 		this.event=d;
 		}
 	
@@ -21,7 +17,10 @@ public class Coffee {
 		System.out.println("이름 \t: "+this.name);
 		System.out.println("분류 \t: "+this.type);
 		System.out.println("판매가격 \t: "+this.price);
-		System.out.println("행사여부 \t: "+this.event);
+		//System.out.println("행사여부 \t: "+this.event);
+		if(this.event) {//this.event==true
+			System.out.println("*할인 가격\t: "+(this.price-500));
+		}
 		System.out.println("--------------------");
 	}
 }
