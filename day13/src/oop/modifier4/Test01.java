26b097983174919fd3eae98eee1fe7740b21d3f4
YR <YR@DESKTOP-HIAR6MN>
2020-10-15 16:50:42 +0900

    day11(getter, import)

--- day13/src/oop/modifier4/Test01.java
+++ day13/src/oop/modifier4/Test01.java
@@ -4,8 +4,17 @@ package oop.modifier4;
 import oop.modifier3.House;
 
 public class Test01 {
-	//다른 패키지(opp.modifier3)에 존재하는 House의 객체를 생성
-	House h1 = new House();
+	public static void main(String[] args) {
 	
+		//다른 패키지(opp.modifier3)에 존재하는 House의 객체를 생성
+		House h1 = new House();
+		
+		//접근 제한자가 없는 경우에는 "같은 패지키" 내에서만 접근 가능
+		//자바에서는 같은 패키지가 기본 인식 단위다(default : package)
+		//다른 패키지의 클래스를 사용하기 위해서는 import를 사용해야함
+		h1.setArea("강남");
 
+		
+	}
+	
 }
