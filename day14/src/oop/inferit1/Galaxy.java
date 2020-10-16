119dc27edc64a6ad65e84d6260ae28acb43a6785
YR <YR@DESKTOP-HIAR6MN>
2020-10-16 19:27:37 +0900

    무의미한 코드

---
+++ day14/src/oop/inferit1/Galaxy.java
@@ -0,0 +1,23 @@
+package oop.inferit1;
+
+public class Galaxy {
+	//멤버 변수
+	private String number;
+	private String color;
+	private int price;
+	
+	//setter&getter 외의 기능(메소드)
+	public void call() {
+		System.out.println("전화 걸기!");
+	}
+	public void sms() {
+		System.out.println("문자 보내기!");
+	}
+	public void camera() {
+		System.out.println("카메라 촬영!");
+	}
+	public void siri() {
+		System.out.println("시리야!");
+	}
+
+}
---
+++ day14/src/oop/inferit1/Test01.java
@@ -0,0 +1,5 @@
+package oop.inferit1;
+
+public class Test01 {
+
+}
