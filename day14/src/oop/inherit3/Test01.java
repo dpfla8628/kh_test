381346b3e5360770675dcaaa8c00810f3e051770
YR <YR@DESKTOP-HIAR6MN>
2020-10-16 21:57:34 +0900

    상속

--- day14/src/oop/inherit3/Test01.java
+++ day14/src/oop/inherit3/Test01.java
@@ -3,11 +3,11 @@ package oop.inherit3;
 public class Test01 {
 	public static void main(String[] args) {
 		//뽀로로 인스턴스 1개 생성
-		Pororo p = new Pororo();
+		Pororo p = new Pororo(null);
 		p.hello();//상속 받은 기능(재정의 됨)
 		p.nag();//교유 기능
 		
-		Tayo t =new Tayo();
+		Tayo t =new Tayo(null);
 		t.hello();
 		t.drive();
 		
