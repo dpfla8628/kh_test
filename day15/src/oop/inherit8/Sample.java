6fdb40040f8be1a73a2d6d3de2eb3f476cb96753
YR <YR@DESKTOP-HIAR6MN>
2020-10-19 19:41:41 +0900

    실제 상속 코드 예시

---
+++ day15/src/oop/inherit8/Sample.java
@@ -0,0 +1,14 @@
+package oop.inherit8;
+
+import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+//자식 클래스
+//창
+//public class Sample extends JFrame{}
+
+//파일
+//public class Sample extends JFileChooser{}
+
+public class Sample extends JColorChooser{}
---
+++ day15/src/oop/inherit8/Test01.java
@@ -0,0 +1,14 @@
+package oop.inherit8;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		
+		//s.setBounds(0,0,400,300);
+		//s.setVisible(true);
+
+		//s.showOpenDialog(null);
+		
+		s.showDialog(null,null,null);
+	}
+}
