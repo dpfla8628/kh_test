4fd00c2459af8f9469358c7f029049f773ddb799
YR <YR@DESKTOP-HIAR6MN>
2020-10-21 21:51:04 +0900

    string예제

---
+++ day17/src/api/lang/string/Test01.java
@@ -0,0 +1,16 @@
+package api.lang.string;
+
+public class Test01 {
+	public static void main(String[] args) {
+		String a = new String("Hello java");
+		String b = new String("hello java");
+		
+		System.out.println(a==b);//같은 객체인지
+		System.out.println(a.equals(b));//값이 같은지
+		System.out.println(a.equalsIgnoreCase(b));//대소문자 무시하고 값 비교
+		System.out.println(a.startsWith("Hello"));//Hello로 시작하는지
+		System.out.println(b.endsWith("java"));//java로 끝나는지
+		System.out.println(a.length());//a글자수
+		System.out.println(b.length());//b글자수
+	}
+}
